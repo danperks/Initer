e6b74f0a7317b082b63a649030132f137f203a20
Dan Perks <49082234+danperks@users.noreply.github.com>
2020-10-04 04:00:11 +0100

    Pushed releases

--- src/update.bat
+++ src/update.bat
@@ -10,19 +10,21 @@ IF %ERRORLEVEL% NEQ 0 (
         del tmpFile
         ECHO Initer version %ver% is successfully installed
         Powershell -Command "wget https://raw.githubusercontent.com/danperks/Initer/master/version -OutFile latestver.txt"
-        set /p latest=<version
+        set /p latest=<latestver.txt
         del latestver.txt
         ECHO Initer latest version is %latest%
         if "%ver%" == "%latest%" (
                 echo You have the latest version already
         ) else (
                 echo You do not have the latest version
-                SET /P AREYOUSURE=Do you want to update now (Y/[N])
-                IF /I "%AREYOUSURE%" NEQ "Y" (
+                SET /P AREYOUSURE=Do you want to update now [y/n]: 
+                IF /I "%AREYOUSURE%" NEQ "Y" ( 
+                        echo Starting Update...
+                        Powershell -Command "wget https://raw.githubusercontent.com/danperks/Initer/master/version -OutFile latestver.txt"
+                        echo Update Complete!
+                ) else (
                         echo Not updating.
                         echo You can manually update at https://github.com/danperks/Initer
-                ) else (
-                        echo Updating
                         )
                 )
 )
